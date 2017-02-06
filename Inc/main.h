dc2ed6a7c4c501449c22c0ca1dc81799137ecb32
MX-Master <mikhail@vydrenko.ru>
2017-02-06 13:58:58 +0600

    1st alpha, not tested yet

---
+++ Inc/main.h
@@ -0,0 +1,99 @@
+/**
+  ******************************************************************************
+  * File Name          : main.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2017 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+  /* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+#define SERVO_CYCLE_TIM_PRESCALER 8
+#define SERVO_CYCLE_TIM_MAX_COUNT 8
+
+#define AXIS1_STEP_OUTPUT_Pin GPIO_PIN_1
+#define AXIS1_STEP_OUTPUT_GPIO_Port GPIOA
+#define AXIS1_DIR_OUTPUT_Pin GPIO_PIN_2
+#define AXIS1_DIR_OUTPUT_GPIO_Port GPIOA
+#define AXIS2_STEP_OUTPUT_Pin GPIO_PIN_3
+#define AXIS2_STEP_OUTPUT_GPIO_Port GPIOA
+#define AXIS2_DIR_OUTPUT_Pin GPIO_PIN_4
+#define AXIS2_DIR_OUTPUT_GPIO_Port GPIOA
+#define AXIS3_STEP_OUTPUT_Pin GPIO_PIN_5
+#define AXIS3_STEP_OUTPUT_GPIO_Port GPIOA
+#define AXIS3_DIR_OUTPUT_Pin GPIO_PIN_6
+#define AXIS3_DIR_OUTPUT_GPIO_Port GPIOA
+#define AXIS3_STEP_INPUT_Pin GPIO_PIN_10
+#define AXIS3_STEP_INPUT_GPIO_Port GPIOB
+#define AXIS3_DIR_INPUT_Pin GPIO_PIN_11
+#define AXIS3_DIR_INPUT_GPIO_Port GPIOB
+#define AXIS4_STEP_INPUT_Pin GPIO_PIN_12
+#define AXIS4_STEP_INPUT_GPIO_Port GPIOB
+#define AXIS4_DIR_INPUT_Pin GPIO_PIN_13
+#define AXIS4_DIR_INPUT_GPIO_Port GPIOB
+#define AXIS5_STEP_INPUT_Pin GPIO_PIN_14
+#define AXIS5_STEP_INPUT_GPIO_Port GPIOB
+#define AXIS5_DIR_INPUT_Pin GPIO_PIN_15
+#define AXIS5_DIR_INPUT_GPIO_Port GPIOB
+#define AXIS4_STEP_OUTPUT_Pin GPIO_PIN_9
+#define AXIS4_STEP_OUTPUT_GPIO_Port GPIOA
+#define AXIS4_DIR_OUTPUT_Pin GPIO_PIN_10
+#define AXIS4_DIR_OUTPUT_GPIO_Port GPIOA
+#define AXIS5_STEP_OUTPUT_Pin GPIO_PIN_11
+#define AXIS5_STEP_OUTPUT_GPIO_Port GPIOA
+#define AXIS5_DIR_OUTPUT_Pin GPIO_PIN_12
+#define AXIS5_DIR_OUTPUT_GPIO_Port GPIOA
+#define AXIS1_STEP_INPUT_Pin GPIO_PIN_6
+#define AXIS1_STEP_INPUT_GPIO_Port GPIOB
+#define AXIS1_DIR_INPUT_Pin GPIO_PIN_7
+#define AXIS1_DIR_INPUT_GPIO_Port GPIOB
+#define AXIS2_STEP_INPUT_Pin GPIO_PIN_8
+#define AXIS2_STEP_INPUT_GPIO_Port GPIOB
+#define AXIS2_DIR_INPUT_Pin GPIO_PIN_9
+#define AXIS2_DIR_INPUT_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+#endif /* __MAIN_H */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
